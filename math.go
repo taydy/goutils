30861214f7966f8cf2ce9747d18f059106529549
部落格上de问候 <taydycn@gmail.com>
2020-02-12 13:49:22 +0800

    feat: math max and min

---
+++ math.go
@@ -0,0 +1,29 @@
+package goutils
+
+// Returns the greater of int values.
+func MaxInt(a ...int) int {
+	if len(a) == 0 {
+		panic("empty data")
+	}
+	max := a[0]
+	for i := 1; i < len(a); i++ {
+		if max < a[i] {
+			max = a[i]
+		}
+	}
+	return max
+}
+
+// Returns the smaller of int values.
+func MinInt(a ...int) int {
+	if len(a) == 0 {
+		panic("empty data")
+	}
+	max := a[0]
+	for i := 1; i < len(a); i++ {
+		if max < a[i] {
+			max = a[i]
+		}
+	}
+	return max
+}
---
+++ math_test.go
@@ -0,0 +1,19 @@
+package goutils
+
+import "testing"
+
+func TestMaxInt(t *testing.T) {
+	a, b, c := 1, 2, 3
+	if max := MaxInt(a, b, c); max != c {
+		t.Logf("except max is %d, but actual is %d", c, max)
+		t.FailNow()
+	}
+}
+
+func TestMinInt(t *testing.T) {
+	a, b, c := 1, 2, 3
+	if min := MaxInt(a, b, c); min != a {
+		t.Logf("except min is %d, but actual is %d", a, min)
+		t.FailNow()
+	}
+}
